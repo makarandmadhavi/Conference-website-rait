7f291eb4e5c372fbdb83a4975df036a9275abcbf
Makarand <makarandmadhavi99@gmail.com>
2020-01-11 16:19:14 +0530

    added tabs

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+function committees() {
+    $(".main").load("committees.html");
+}
+
+function home() {
+    $(".main").load("home.html");
+}
+
+function paper() {
+    $(".main").load("paper.html");
+}
+
+function speaker() {
+    $(".main").load("speaker.html");
+}
+
+function author() {
+    $(".main").load("author.html");
+}
+
+function venue() {
+    $(".main").load("venue.html");
+}
+
+function contact() {
+    $(".main").load("contact.html");
+}
