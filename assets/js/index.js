c13e01a95f364b804c85ef80ae8c561c87aa377b
Makarand <makarandmadhavi99@gmail.com>
2020-01-12 11:15:01 +0530

    cleaned code and css changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,5 @@
-function committees() {
-    $(".main").load("committees.html");
-}
-
-function home() {
-    $(".main").load("home.html");
-}
-
-function paper() {
-    $(".main").load("paper.html");
-}
-
-function speaker() {
-    $(".main").load("speaker.html");
-}
-
-function author() {
-    $(".main").load("author.html");
-}
-
-function venue() {
-    $(".main").load("venue.html");
-}
-
-function contact() {
-    $(".main").load("contact.html");
+function changetab(id) {
+    $(".main").load(id+".html");
+    $("li").removeClass("active");
+    $("#"+id).addClass("active");
 }
